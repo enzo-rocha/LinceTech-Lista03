4ab2587e64af10445e37fdad333a3fff145c60ba
Enzo Rocha <106124179+enzo-rocha@users.noreply.github.com>
2022-10-10 19:47:26 -0300

    Update ex03.dart

--- ex03.dart
+++ ex03.dart
@@ -33,14 +33,16 @@ void main() {
   
   final int tamanho = nomes.length;
   
-  for (int i = 0; i < tamanho; i++) {
+  // Estrutura que coloca e mostra os clientes da lista com numeração
+  for (var i = 0; i < tamanho; i++) {
     cliente.gerarNomeAleatorio(nomes, sobrenomes, clientes, tamanho);
     print("Cliente ${i + 1}: ${clientes.elementAt(i)} entrou na fila!");
   }
   
   print("-------------------------------------------");
   
-  for (int i = 0; i < tamanho; i++) {
+  // Atende os clientes conforme sua vez na fila (posição no Set)
+  for (var i = 0; i < tamanho; i++) {
     print("${clientes.elementAt(i)} foi atendido(a)");
   }
   
@@ -49,6 +51,9 @@ void main() {
 }
 
 class Nome {
+  
+  // Gera um nome e um sobrenome aleatorio e adiciona na fila
+  // de clientes
   gerarNomeAleatorio(Set<String> nome, Set<String> sobrenome, Set<String> cliente, int tamanho) {
     String n = nome.elementAt(Random().nextInt(tamanho));
     String s = sobrenome.elementAt(Random().nextInt(tamanho));
