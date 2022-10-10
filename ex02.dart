7fe9106dcd77ab7c239c350ca5e48d10d4c7bb23
Enzo Rocha <106124179+enzo-rocha@users.noreply.github.com>
2022-10-10 19:03:32 -0300

    Update ex02.dart

--- ex02.dart
+++ ex02.dart
@@ -3,32 +3,36 @@ void main() {
 
   final Set<String> cartas = {"Paus", "Copas", "Espadas", "Ouro"};
 
-  truco.montaBaralho(truco.baralho, cartas);
+  truco.montaBaralho(truco._baralho, cartas);
   truco.mostraBaralho();
 
-  for (int i = 0; i < truco.baralho.length; i++) {
-    if (truco.baralho.elementAt(i).contains(cartas.first)) {
+  // Estrutura responsável por remover a primeira carta do baralho
+  for (var i = 0; i < truco._baralho.length; i++) {
+    if (truco._baralho.elementAt(i).contains(cartas.first)) {
       print("Removendo carta do baralho...");
-      truco.baralho.remove(truco.baralho.elementAt(i));
+      truco._baralho.remove(truco._baralho.elementAt(i));
     }
   }
 
   truco.mostraBaralho();
-  truco.adicionaCartaRemovidaUltimo(truco.baralho, cartas);
+  truco.adicionaCartaRemovidaUltimo(truco._baralho, cartas);
   truco.mostraBaralho();
 }
 
 class Baralho {
-  final Set<String> baralho = {};
+  final Set<String> _baralho = {};
 
-  mostraBaralho() => print("As cartas do baralho são: $baralho");
+  // Mostra o baralho com as cartas adicionadas
+  mostraBaralho() => print("As cartas do baralho são: $_baralho");
 
+  // Monta o baralho partir das cartas denominadas no main
   montaBaralho(Set<String> baralho, Set<String> cartas) {
-    for (int i = 0; i < cartas.length; i++) {
+    for (var i = 0; i < cartas.length; i++) {
       baralho.add(cartas.elementAt(i));
     }
   }
   
+  // Pega a carta removida e coloca na última posição do baralho
   adicionaCartaRemovidaUltimo(Set<String> baralho, Set<String> cartas) {
     print("Adicionando carta removida embaixo do baralho...");
     baralho.add(cartas.first);
